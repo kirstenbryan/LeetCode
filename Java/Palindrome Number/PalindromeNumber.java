066771149bbd1b74b9d299fb8780f7bf0dcffa4d
kirstenbryan <kirstebs@miamioh.edu>
2017-03-30 21:08:23 -0400

    Update PalindromeNumber.java

--- Java/Palindrome Number/PalindromeNumber.java
+++ Java/Palindrome Number/PalindromeNumber.java
@@ -1,8 +1,11 @@
 public class Solution {
     public boolean isPalindrome(int x) {
+        
         String num = Integer.toString(x);
+        
         if(x < 0) 
             return false;
+        
         for(int i = 0; i < num.length() / 2; i++){
             char d1 = num.charAt(i);
             char d2 = num.charAt(num.length() - i - 1);

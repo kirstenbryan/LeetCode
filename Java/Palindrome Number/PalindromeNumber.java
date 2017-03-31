46c0b73a71f594dcd5118e785b4e3ec0b9b92afe
kirstenbryan <kirstebs@miamioh.edu>
2017-03-30 20:58:57 -0400

    Create PalindromeNumber.java

---
+++ Java/Palindrome Number/PalindromeNumber.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        String num = Integer.toString(x);
+        if(x < 0) 
+            return false;
+        for(int i = 0; i < num.length() / 2; i++){
+            char d1 = num.charAt(i);
+            char d2 = num.charAt(num.length() - i - 1);
+            if(d1 != d2)
+                return false;
+        }
+        return true;
+    }
+}
